b0931c933a656ddee336cf4d5ea5edcd9da273df
diegofcornejo <diegof.cornejo@gmail.com>
2021-10-24 20:43:46 -0600

    Initial commit

--- engineer.config.js
+++ engineer.config.js
@@ -1,13 +1,27 @@
-module.exports =  () => {
-  return {
-    data: {
-      message: 'hello world!',
-    },
-    templates: [
-        {
-          src: '.satelite/engineer/files/index.js',
-          dest: 'src/index.js',
-        },
-      ],
-  };
+module.exports = () => {
+	return {
+		data: {
+			"FunctionName": "lambda-github-actions",
+			"Description": "Engineer Lambda CI with github actions",
+			"Role": "GithubActionsRole",
+			"Handler": "index.handler",
+			"Runtime": "nodejs14.x",
+			"Timeout": 3,
+			"MemorySize": 128
+		},
+		templates: [
+			{
+				src: '.satelite/engineer/files/lambda.config.json',
+				dest: 'src/lambda.config.json',
+			},
+			{
+				src: '.satelite/engineer/files/package.json',
+				dest: 'src/package.json',
+			},
+			{
+				src: '.satelite/engineer/files/template.yaml',
+				dest: 'src/template.yaml',
+			},
+		],
+	};
 };
